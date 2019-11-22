c967c115bfe0fb1a5d9a7a13ca656fd9add49678
Brandon Rizzo <bnrizzo86@gmail.com>
2019-11-22 13:27:00 -0600

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,57 @@
+provider "random" {
+  version = "~> 2.0"
+}
+
+provider "template" {
+  version = "~> 2.0"
+}
+
+# Use AWS Terraform provider
+provider "aws" {
+  version             = "~> 2.1"
+  region              = "${var.region}"
+  allowed_account_ids = ["${var.aws_account_id}"]
+}
+
+terraform {
+  backend "s3" {
+    # this key must be unique for each layer!
+    key     = "terraform.pipeline.tfstate"
+    region  = "us-east-1"
+    encrypt = "true"
+  }
+}
+
+# Create EC2 instance
+resource "aws_instance" "default" {
+  ami                    = "${var.ami}"
+  count                  = "${var.counts}"
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  source_dest_check      = false
+  instance_type          = "${var.instance_type}"
+
+  tags {
+    Name = "terraform-default"
+  }
+}
+
+# Create Security Group for EC2
+resource "aws_security_group" "default" {
+  name = "terraform-default-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "counts" {
+  default = 1
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "key_name" {
+  description = "Private key name to use with instance"
+  default     = "dev"
+}
+
+variable "instance_type" {
+  description = "AWS instance type"
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "Base AMI to launch the instances"
+
+ 
+  default = "ami-00068cd7555f543d5"
+}
