a0f665a45c2276eeaacd458e58f698d2e9912c41
Brandon Rizzo <bnrizzo86@gmail.com>
2019-11-22 13:50:43 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,9 +31,7 @@ resource "aws_instance" "default" {
   source_dest_check      = false
   instance_type          = "${var.instance_type}"
 
-  tags {
-    Name = "terraform-default"
-  }
+  
 }
 
 # Create Security Group for EC2
